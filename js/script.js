1f3fe7e6ab6aa44248dc69ddf873d42b36e7c222
Saumaybrata <61882443+Saumaybrata@users.noreply.github.com>
2020-06-28 16:35:25 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -114,7 +114,7 @@ $ajaxUtils.sendGetRequest(
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-      insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
+      insertHtml("#home-tiles",homeHtmlToInsertIntoMainPage);
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
